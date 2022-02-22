2012d4c70b8c0760e0e67b2d9ea2d489d1a64de2
Cissy <91531100+cissyverhaeghe@users.noreply.github.com>
2022-02-22 12:24:12 +0100

    chapter 9 done

--- lib/ShipLoader.php
+++ lib/ShipLoader.php
@@ -4,6 +4,20 @@ class ShipLoader
 {
     private $pdo;
 
+    private $dbDsn;
+
+    private $dbUser;
+
+    private $dbPass;
+
+    public function __construct($dbDsn, $dbUser, $dbPass)
+    {
+        $this->dbDsn = $dbDsn;
+        $this->dbUser = $dbUser;
+        $this->dbPass = $dbPass;
+    }
+
+
     /**
      * @return Ship[]
      */
@@ -65,7 +79,7 @@ class ShipLoader
     private function getPDO()
     {
         if ($this->pdo === null) {
-            $pdo = new PDO('mysql:host=localhost;dbname=oo_battle', 'root', 'root');
+            $pdo = new PDO($this->dbDsn, $this->dbUser, $this->dbPass);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
             $this->pdo = $pdo;
