fb3e83e7ac95de20771c954f4e02aff8883b741f
Cissy <91531100+cissyverhaeghe@users.noreply.github.com>
2022-02-22 12:58:52 +0100

    chapter 12 done

--- lib/Container.php
+++ lib/Container.php
@@ -4,6 +4,12 @@ class Container
 {
     private $configuration;
 
+    private $pdo;
+
+    private $shipLoader;
+
+    private $battleManager;
+
     public function __construct(array $configuration)
     {
         $this->configuration = $configuration;
@@ -14,8 +20,33 @@ class Container
      */
     public function getPDO()
     {
-        $pdo = new PDO($this->configuration['db_dsn'], $this->configuration['db_user'], $this->configuration['db_pass']);
+        if ($this->pdo === null) {
+            $this->pdo = new PDO($this->configuration['db_dsn'], $this->configuration['db_user'], $this->configuration['db_pass']);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
 
-        return $pdo;
+        return $this->pdo;
+    }
+
+    /**
+     * @return ShipLoader
+     */
+    public function getShipLoader()
+    {
+        if($this->shipLoader === null){
+            $this->shipLoader = new ShipLoader($this->getPDO());
+        }
+        return $this->shipLoader;
+    }
+
+    /**
+     * @return BattleManager
+     */
+    public function getBattleManager()
+    {
+        if($this->battleManager === null){
+            $this->battleManager = new BattleManager();
+        }
+        return $this->battleManager;
     }
 }
